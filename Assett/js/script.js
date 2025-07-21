939caf630122ebc01288a2dda72861cb28f15f68
Lakshya <lakshu@Lakshyas-MacBook-Air.local>
2025-07-21 17:12:26 +0530

    first commit

---
+++ Assett/js/script.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, {
+  threshold: 0.1,
+});
+
+document.querySelectorAll(".movie-card, .movie-thumb").forEach((el) => {
+  el.classList.add("fade-in");
+  observer.observe(el);
+});
